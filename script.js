6a9f2c5523778e52b21d753c10ca046c4e3589fb
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-23 11:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async ()=>{
+
+	let text=document.getElementById("text").value;
+	let delay=document.getElementById("delay").value;
+
+	if (text && delay) {
+		let myPromise=Promise((resolve) =>{
+			setTimeout(()=>{
+				resolve(text)
+			},delay)
+		})
+
+		let text2=await myPromise
+
+		if (text2) {
+			document.getElementById("output").textContent=text2
+		}
+	}
+})
